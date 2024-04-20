d2b9274ccc29f9f1c5e060ae7e78c2df17b15e95
Water The People <66826331+WaterThePeople@users.noreply.github.com>
2024-04-20 13:29:26 +0200

    #backend_req

--- frontend/src/utils/getData.tsx
+++ frontend/src/utils/getData.tsx
@@ -3,22 +3,22 @@ import axios from "axios";
 
 const [data, setData] = useState<any[]>([]);
 
-  useEffect(() => {
-    getData();
-  }, []);
+useEffect(() => {
+  getData();
+}, []);
 
 function getData() {
-    axios
-      .get("/api/TestItems/")
-      .then((response) => {
-        const data = response.data;
-        setData(data);
-      })
-      .catch((error) => {
-        if (error.response) {
-          console.log(error.response);
-          console.log(error.response.status);
-          console.log(error.response.headers);
-        }
-      });
-  }
+  axios
+    .get("/api/TestItems/")
+    .then((response) => {
+      const data = response.data;
+      setData(data);
+    })
+    .catch((error) => {
+      if (error.response) {
+        console.log(error.response);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+      }
+    });
+}
