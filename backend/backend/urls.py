1fd3812b9dcc97b1325a687f49a82c81e89738c9
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-05-27 21:05:20 +0200

    Dodano Filtrowanie(totalPrice i rangeprice)
    Metody filtrowania powinny działać tak samo. Do zostawienia wygodniejszą wersję

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,6 +7,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/home',views.HomePageView.as_view()),
-    path('api/modDel/<pk>',views.SingleModDelGameView.as_view()),
-    path('api/add/<pk>',views.AddGameView.as_view()),
+    path('api/allGame/modDel/<pk>',views.SingleModDelGameView.as_view()),
+    path('api/allGame/add/<pk>',views.AddGameView.as_view()),
+    path('api/allGame',views.AllGamesView.as_view())
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -3,21 +3,38 @@ from rest_framework import viewsets, generics
 from .serializers import HomeGameSerializer, AllGameSerializer
 from .models import Game
 from django_filters.rest_framework import DjangoFilterBackend
+from django_filters import rest_framework
 
 # Create your views here.
+
+class NumberRangeFilter(rest_framework.BaseRangeFilter, rest_framework.NumberFilter):
+    pass
+class FullListFilterSet(rest_framework.FilterSet):
+#    def to_html(self, request, queryset, view):
+    totalprice = rest_framework.NumericRangeFilter(field_name='price',lookup_expr='range')
+    rangeprice = NumberRangeFilter(field_name='price',lookup_expr='range',label="price-range")
+    class Meta:
+        model = Game
+        fields = ['recommended','new','bestsellers','sale','popular']
 
 class HomePageView(generics.ListAPIView):
     serializer_class = HomeGameSerializer
     queryset = Game.objects.all()
     filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['recommended','new','bestsellers']
+    filterset_fields = ['recommended','new','bestsellers','sale','popular']
 
 class SingleModDelGameView(generics.RetrieveUpdateDestroyAPIView):
-    serializer_class = HomeGameSerializer
+    serializer_class = AllGameSerializer
     queryset = Game.objects.all()
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['recommended','new','bestsellers']
+#    filter_backends = [DjangoFilterBackend]
+#    filterset_fields = ['recommended','new','bestsellers']
 
 class AddGameView(generics.CreateAPIView):
     serializer_class = AllGameSerializer
     queryset = Game.objects.all()
+
+class AllGamesView(generics.ListAPIView):
+    serializer_class = AllGameSerializer
+    queryset = Game.objects.all()
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = FullListFilterSet
