956062b9f8b21aa8eb70db097ba83d64375115f0
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-05-30 13:33:52 +0200

    Added new filters

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -4,6 +4,7 @@ from .serializers import HomeGameSerializer, AllGameSerializer
 from .models import Game
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import rest_framework
+from django.db.models import F
 
 # Create your views here.
 
@@ -11,11 +12,16 @@ class NumberRangeFilter(rest_framework.BaseRangeFilter, rest_framework.NumberFil
     pass
 class FullListFilterSet(rest_framework.FilterSet):
 #    def to_html(self, request, queryset, view):
-    totalprice = rest_framework.NumericRangeFilter(field_name='price',lookup_expr='range')
-    rangeprice = NumberRangeFilter(field_name='price',lookup_expr='range',label="price-range")
+    pricerange = NumberRangeFilter(field_name='price',lookup_expr='range',label="price-range")
+    discounted_price = NumberRangeFilter(method='filter_discounted_price',label="discounted-price-range")
+    def filter_discounted_price(self, queryset, name, value):
+        # Annotate the queryset with the calculated field
+        queryset = queryset.annotate(discounted_price_calc=F('price') - F('price') * F('discount'))
+        return queryset.filter(discounted_price_calc__range=value)
+
     class Meta:
         model = Game
-        fields = ['recommended','new','bestsellers','sale','popular']
+        fields = ['recommended','new','bestsellers','sale','popular','category','budget','platform']
 
 class HomePageView(generics.ListAPIView):
     serializer_class = HomeGameSerializer
