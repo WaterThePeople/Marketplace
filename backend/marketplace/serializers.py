7f1720e2db86835a0c0d1bafe74d005e3ca30b0b
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2024-06-04 13:19:18 +0200

    Add category

--- backend/marketplace/serializers.py
+++ backend/marketplace/serializers.py
@@ -6,12 +6,12 @@ from django.contrib.auth.models import User
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = ['category_name']
+        fields = ['id','category_name']
 
 class PlatformSerializer(serializers.ModelSerializer):
     class Meta:
         model = Platform
-        fields = ['platform_name']
+        fields = ['id','platform_name']
 
 class HomeGameSerializer(serializers.ModelSerializer):
     discount_price = serializers.SerializerMethodField('get_discount_price')
@@ -33,7 +33,6 @@ class AllGameSerializer(serializers.ModelSerializer):
     sale = serializers.SerializerMethodField()
     owner = serializers.ReadOnlyField(source='owner.username')
     discount_price = serializers.SerializerMethodField('get_discount_price')
-    image = serializers.ImageField()
     def get_sale(self,obj):
         return obj.discount != 0
     def get_category(self, obj):
@@ -49,6 +48,33 @@ class AllGameSerializer(serializers.ModelSerializer):
     class Meta:
         model = Game
         fields = ('id', 'name', 'price','discount','discount_price', 'image','recommended','new','sale','year','category','budget','developer','platform','owner')
+
+class AddGameSerializer(serializers.ModelSerializer):
+    category = serializers.PrimaryKeyRelatedField(queryset=Category.objects.all(), many=True)
+    platform = serializers.PrimaryKeyRelatedField(queryset=Platform.objects.all(), many=True)
+    image = serializers.ImageField()
+    class Meta:
+        model = Game
+        fields =  ('name', 'price','discount', 'image','year','category','budget','developer','platform','owner')
+    def create(self, validated_data):
+        categories_data = validated_data.pop('category')
+        platforms_data = validated_data.pop('platform')
+        game = Game.objects.create(**validated_data)
+        game.category.set(categories_data)
+        game.platform.set(platforms_data)
+        return game
+
+    def update(self, instance, validated_data):
+        categories_data = validated_data.pop('category', None)
+        platforms_data = validated_data.pop('platform', None)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        if categories_data is not None:
+            instance.category.set(categories_data)
+        if platforms_data is not None:
+            instance.platform.set(platforms_data)
+        instance.save()
+        return instance
 
 class UserSerializer(serializers.ModelSerializer):
     submissions = serializers.PrimaryKeyRelatedField(many=True, queryset=Game.objects.all())
